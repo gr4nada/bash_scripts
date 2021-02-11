308e31418ad40cf5245e27af175b92d71a4ce719
dparaiso <danapaum@gmail.com>
2021-02-11 00:04:12 -0300

    criando o script de save_log e o show_scenarios

---
+++ jenkins/save_log.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+log_path="logs"
+
+      
+function save_log() {
+  echo "$(date +%T) - $log_level - $msg" >> $log_path/"$time_stamp"_api.log
+}
+
+function create_header(){
+  echo "" >> $log_path/"$time_stamp"_api.log
+  echo "$(date +%T) - [$msg] ==========================================" >> $log_path/"$time_stamp"_api.log
+}
+
+function help() {
+  cat<<-EOM
+  NAME:
+      save_log - Create the system log output.
+
+  USAGE:
+      save_log <TYPE> <MESSAGE>
+
+  DESCRIPTION:
+      Save information about system on the logfile.
+
+      Options:
+        TYPE  [EVENT|INFO|ERROR|WARN] 
+                EVENT -  Create a tag event on LOG. The message in this case is a event title. 
+                INFO - Log INFO level.
+                ERROR - Log ERROR level.
+                WARN - Log WARN level.
+EOM
+}
+
+# start variables
+time_stamp=""
+count=1
+log_level=""
+
+# test to validate if there are any arguments
+if [[ -z "$1" ]]; then
+  help
+fi
+
+# correctly getting the input strings
+for arg in "$@"; do
+  if [ "1" -eq "$count" ]; then
+          log_level=$arg
+  fi
+  if [ "2" -eq "$count" ]; then
+          msg=$arg
+  fi
+  ((count++))
+done
+
+mkdir -p $log_path
+time_stamp=$(date +%Y-%m-%d)
+
+case $log_level in
+  EVENT) create_header $msg;;
+  INFO) save_log $msg;;
+  ERROR) save_log $msg;;
+  WARN) save_log $msg;;
+  *) help;;
+esac
---
+++ jenkins/show_scenarios.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+function read_file(){    
+    while IFS=$':' read name parameters description;
+    do echo "$name - $description"; 
+    done < scenarios.tx
+}
+
+function help() {
+ cat<<-EOM
+  NAME:
+      show_scenario - show the scenarios log output.
+
+  USAGE:
+      show_scenario
+      show_scenario [help|--help] 
+EOM
+}
+
+case $1 in
+  help) help;;
+  --help) help;;
+  *) read_file;;
+esac
